795cf666c64f29b8ad20d0321d9f829623cc70ed
chuhongliang <13717807729@163.com>
2019-12-31 23:03:07 +0800

    1.0

---
+++ test/test.js
@@ -0,0 +1,11 @@
+let accountSid = '8a216da869';
+let appId = '8a216da869';
+let token = '56c3cd';
+const sms = require('../lib/sms')(accountSid, appId, token);
+
+async function test(){
+	let result = await sms.send('13717807729', 332870, '1',1);
+	console.log(result);
+}
+
+test();
