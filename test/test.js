a03dae6f8755377856496f3eb5f29ffbcc0fb0c4
chuhongliang <13717807729@163.com>
2020-01-01 07:10:41 +0800

    优化传参

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-const accountSid = '8a216da869';
-const appId = '8a216da869';
-const token = '56c3cd';
+const accountSid = '8a216da86904c0600';
+const appId = '8a216da86904';
+const token = '56c3cd7d6529';
 const sms = require('../lib/sms')(accountSid, appId, token);
 
 async function test() {
-	let result = await sms.send('1561902230', 332870, '1', 1);
+	let result = await sms.send('1561902230', '1', [332870,1]);
 	console.log(result);
 }
 test();
